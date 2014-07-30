c94a7802b601d05555fcbd8357f4ed5f091d37ba
C.W. Betts <computers57@hotmail.com>
2014-07-29 20:16:54 -0600

    Initial work on the Swift test app.

---
+++ Swift Test App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift Test App
+//
+//  Created by C.W. Betts on 7/29/14.
+//  Copyright (c) 2014 Sparkle Project. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Swift Test App/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swift Test App
+//
+//  Created by C.W. Betts on 7/29/14.
+//  Copyright (c) 2014 Sparkle Project. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
